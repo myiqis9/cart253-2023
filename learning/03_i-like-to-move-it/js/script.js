fa16d9dc602a3d83285ea911ffe6ca258bdc87d2
Ema Radu <emv.rad@gmail.com>
2023-09-21 16:59:51 -0400

    exercise 1 finished YAYYYYY

--- learning/03_i-like-to-move-it/js/script.js
+++ learning/03_i-like-to-move-it/js/script.js
@@ -19,6 +19,9 @@ let layer1 = 200;
 let layer2 = 310;
 let layer3 = 420;
 
+let wave = 0;
+let waveDirection = false;
+
 let up = 0.0;
 let down = 0.0;
 let upY = 0;
@@ -64,6 +67,21 @@ function draw() {
             cloud4X = 760;
         }
 
+        if(wave == 40) {
+            waveDirection = true;
+        }
+        
+        if(wave == 0) {
+            waveDirection = false;
+        }
+
+        if(waveDirection) {
+            wave--;
+        }
+        else {
+            wave++;
+        }
+
         cloudtimer = 25;
     }
 
@@ -104,6 +122,8 @@ function draw() {
     //variable constraints
     upY = map(mouseY, windowHeight, 0, 0, 65);
     downY = map(mouseY, windowHeight, 0, 0, 65);
+    upY = constrain(upY, 0, 65);
+    downY = constrain(downY, 0, 65);
 
     //assign up and down
     up = layer1 + upY;
@@ -114,13 +134,13 @@ function draw() {
     //waves
     strokeWeight(9);
     //row 1
-    bezier(-80, 200, -80, down, 30, down, 30, 200);
-    bezier(30, 200, 30, up, 140, up, 140, 200);
-    bezier(140, 200, 140, down, 250, down, 250, 200);
-    bezier(350, 200, 350, up, 250, up, 250, 200);
-    bezier(460, 200, 460, down, 350, down, 350, 200);
-    bezier(570, 200, 570, up, 460, up, 460, 200);
-    bezier(680, 200, 680, down, 570, down, 570, 200);
+    bezier(-80+wave, 200, -80+wave, down, 30+wave, down, 30+wave, 200);
+    bezier(30+wave, 200, 30+wave, up, 140+wave, up, 140+wave, 200);
+    bezier(140+wave, 200, 140+wave, down, 250+wave, down, 250+wave, 200);
+    bezier(350+wave, 200, 350+wave, up, 250+wave, up, 250+wave, 200);
+    bezier(460+wave, 200, 460+wave, down, 350+wave, down, 350+wave, 200);
+    bezier(570+wave, 200, 570+wave, up, 460+wave, up, 460+wave, 200);
+    bezier(680+wave, 200, 680+wave, down, 570+wave, down, 570+wave, 200);
 
     //variable constraints
     up = layer2 + upY;
@@ -129,23 +149,33 @@ function draw() {
     
     
     //row 2
-    bezier(-20, 310, -20, down, 90, down, 90, 310);
-    bezier(90, 310, 90, up, 200, up, 200, 310);
-    bezier(310, 310, 310, down, 200, down, 200, 310);
-    bezier(430, 310, 430, up, 310, up, 310, 310);
-    bezier(540, 310, 540, down, 430, down, 430, 310);
-    bezier(650, 310, 650, up, 540, up, 540, 310);
+    bezier(-20-wave, 310, -20-wave, down, 90-wave, down, 90-wave, 310);
+    bezier(90-wave, 310, 90-wave, up, 200-wave, up, 200-wave, 310);
+    bezier(310-wave, 310, 310-wave, down, 200-wave, down, 200-wave, 310);
+    bezier(430-wave, 310, 430-wave, up, 310-wave, up, 310-wave, 310);
+    bezier(540-wave, 310, 540-wave, down, 430-wave, down, 430-wave, 310);
+    bezier(650-wave, 310, 650-wave, up, 540-wave, up, 540-wave, 310);
 
     //variable constraints
     up = layer3 + upY;
     down = layer3 - downY;
     
     //row 3
-    bezier(30, 430, 30, up, -90, up, -90, 430);
-    bezier(150, 430, 150, down, 30, down, 30, 430);
-    bezier(260, 430, 260, up, 150, up, 150, 430);
-    bezier(380, 430, 380, down, 260, down, 260, 430);
-    bezier(490, 430, 490, up, 380, up, 380, 430);
-    bezier(610, 430, 610, down, 490, down, 490, 430);
+    bezier(30+wave, 430, 30+wave, up, -90+wave, up, -90+wave, 430);
+    bezier(150+wave, 430, 150+wave, down, 30+wave, down, 30+wave, 430);
+    bezier(260+wave, 430, 260+wave, up, 150+wave, up, 150+wave, 430);
+    bezier(380+wave, 430, 380+wave, down, 260+wave, down, 260+wave, 430);
+    bezier(490+wave, 430, 490+wave, up, 380+wave, up, 380+wave, 430);
+    bezier(610+wave, 430, 610+wave, down, 490+wave, down, 490+wave, 430);
 
+    let fishColor = map(mouseY, 0, width, 100, 200);
+
+    //fish!
+    strokeWeight(0);
+    fill(255,fishColor,fishColor);
+    ellipse(mouseX, mouseY, 60, 30);
+    triangle(mouseX-10, mouseY-10, mouseX+20, mouseY, mouseX+10, mouseY-30);
+    triangle(mouseX+20, mouseY, mouseX+50, mouseY-18, mouseX+50, mouseY+18);
+    fill(0,0,0);
+    ellipse(mouseX-16, mouseY-5, 6, 4);
 }
