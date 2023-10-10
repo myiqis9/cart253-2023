21140d765d56c746b64c32f19ef528fec2c0a9cb
Ema Radu <83728442+myiqis9@users.noreply.github.com>
2023-10-10 02:29:31 -0400

    part 1 of project done
    created classes and objects, setup initial burrows, foxes spawn, have working peek and walk states, and delete if they're out of bounds. game states work, gameOver if no more lives. drew placeholder sprites for everything to show.
    
    next major things to get done:
    - checkDifficulty function increases fox speed and spawn rate the higher your score is
    - create the food menu and the ability to drag them around, foxes interact if in walk state -> eat state. if used food goes on cooldown
    - foxes go from eat to wait state in which you have to click them to pet them
    - foxes burrow a new hole in the ground after being pet OR if already in the vicinity of a hole burrow in the existing one
    - regain lives if you feed foxes the feast?
    - extra interactivity to regain lives?? rare event happens on screen?
    - bettering the game aesthetics

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -5,26 +5,73 @@
 
 "use strict";
 
-class Fox {
-  constructor(x, y, state, goLeft, speed) {
-    this.x = x;
-    this.y = y;
-    this.state = state; //peek, walk, eat, wait, pet, burrow
-    this.goLeft = goLeft; //direction fox is going
-    this.speed = speed; //speed of the fox
-    this.petTimer = 30;
-    this.size = 100;
+  class Burrow {
+    constructor(x, y) {
+      this.x = x;
+      this.y = y;
+      this.canSpawn = false; //is spawning fox
+      this.size = 150;
+    }
   }
-}
 
-class Burrow {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.canSpawn = false; //is spawning fox
-    this.size = 150;
+  class Fox {
+    constructor(id, x, y, goLeft, speed) {
+      this.id = id;
+      this.x = x;
+      this.y = y;
+      this.state = `peek`; //peek, walk, eat, wait, pet, burrow
+      this.goLeft = goLeft; //direction fox is going
+      this.speed = speed; //speed of the fox
+      this.actionTimer = 40;
+      this.waitTimer = 40;
+      this.size = 100;
+    }
+  
+    peeking() {
+      if(this.actionTimer !== 0) {
+        this.actionTimer--;
+      }
+      else {
+        this.actionTimer = 40;
+        this.state = `walk`;
+      }
+    }
+  
+    walking() {
+      if(this.goLeft == true) {
+        this.x = this.x - this.speed;
+        if(this.x <= 25) {
+          //fox out of bounds, lose health
+          lives--;
+          deleteFox(this.id);
+        }
+      }
+      else {
+        this.x = this.x + this.speed;
+        if(this.x >= (width-25)) {
+          //fox out of bounds, lose health
+          lives--;
+          deleteFox(this.id);
+        }
+      }
+    }
+  
+    eating() {
+  
+    }
+  
+    waiting() {
+  
+    }
+  
+    petting() {
+  
+    }
+  
+    burrowing() {
+  
+    }
   }
-}
 
   let bgColor = {
     R: 0,
@@ -37,6 +84,7 @@ class Burrow {
     y: undefined,
     size: 50,
     cooldown: false,
+    isDragged: false,
     cdTime: 20
   }
 
@@ -45,6 +93,7 @@ class Burrow {
     y: undefined,
     size: 50,
     cooldown: false,
+    isDragged: false,
     cdTime: 20
   }
 
@@ -53,6 +102,7 @@ class Burrow {
     y: undefined,
     size: 50,
     cooldown: false,
+    isDragged: false,
     cdTime: 40
   }
   
@@ -63,17 +113,29 @@ class Burrow {
 
   let foxes = []; //create array of foxes
   let burrows = []; //create array of burrows
-  let foxSpeed = 5;
+  let foxSpeed = 1.5;
+
+  let foxID = 0; //assign a number to foxes for identification
+  let spawnCounter = 40; //cooldown until another fox is spawned
+  let activeFoxes = 0; //number of foxes on screen
+  let maxFoxes = 4; // max number of foxes that can spawn
 
   let score = 0; //total score
   let lives = 10; //foxes being sad makes it decrease, too many sad foxes and you lose!
 
   //images for everything
-  let foxPeek;
-  let foxWalk;
-  let foxWait;
-  let foxPet;
-  let foxBurrow;
+  let foxPeekL;
+  let foxPeekR;
+  let foxWalkL;
+  let foxWalkR;
+  let foxEatL;
+  let foxEatR;
+  let foxWaitL;
+  let foxWaitR;
+  let foxPetL;
+  let foxPetR;
+  let foxBurrowL;
+  let foxBurrowR;
   let burrowImg;
   let food1Img;
   let food2Img;
@@ -83,17 +145,38 @@ class Burrow {
 
   function preload() {
     //load images
-    //foxPeek = loadImage("");
+    burrowImg = loadImage("assets/images/burrow.png");
+    foxPeekL = loadImage("assets/images/fpeekL.png");
+    foxPeekR = loadImage("assets/images/fpeekR.png");
+    foxWalkL = loadImage("assets/images/fwalkL.png");
+    foxWalkR = loadImage("assets/images/fwalkR.png");
+    foxEatL = loadImage("assets/images/featL.png");
+    foxEatR = loadImage("assets/images/featR.png");
+    foxWaitL = loadImage("assets/images/fwaitL.png");
+    foxWaitR = loadImage("assets/images/fwaitR.png");
+    foxPetL = loadImage("assets/images/fpetL.png");
+    foxPetR = loadImage("assets/images/fpetR.png");
+    foxBurrowL = loadImage("assets/images/fburrowL.png");
+    foxBurrowR = loadImage("assets/images/fburrowR.png");
   }
   
   function setup() {
-    createCanvas(950,750);
+    createCanvas(975,800);
 
     setupBurrows();
   }
   
   function setupBurrows() {
     //spawn burrows at the start of the game
+    let burrow1 = new Burrow(random(50, width-50), (height/2)-350);
+    let burrow2 = new Burrow(random(50, width-50), (height/2)-250);
+    let burrow3 = new Burrow(random(50, width-50), (height/2)-150);
+    let burrow4 = new Burrow(random(50, width-50), (height/2)-50);
+    let burrow5 = new Burrow(random(50, width-50), (height/2)+50);
+    let burrow6 = new Burrow(random(50, width-50), (height/2)+150);
+    let burrow7 = new Burrow(random(50, width-50), (height/2)+250);
+
+    burrows.push(burrow1, burrow2, burrow3, burrow4, burrow5, burrow6, burrow7);
   }
   
   function draw() {
@@ -115,11 +198,11 @@ class Burrow {
   function title() {
     push();
     textSize(64);
-    fill(255,150,150);
+    fill(255);
     textAlign(CENTER,CENTER);
     text(`FOX COLLECTOR!!`,width/2,height/2);
 
-    fill(200,100,100);
+    fill(200);
     textSize(32);
     text(`Feed the foxes and raise a happy fox family!`, width/2, (height*2)/3);
     text(`Press ENTER to start.`, width/2, (height*2)/3+38);
@@ -128,8 +211,88 @@ class Burrow {
   
   function simulation() {
     //simulation functions go here
-    
+    checkSpawnTime();
+    checkFoxBehavior();
+    checkDifficulty();
+    checkLives();
     display();
+  }
+
+  function checkSpawnTime() {
+    if(spawnCounter === 0) {
+      if(activeFoxes !== maxFoxes) {
+        spawnFox();
+      }
+
+      //the more the game goes on the faster they spawn
+      spawnCounter = 40;
+    }
+    else {
+      spawnCounter--;
+    }
+  }
+
+  function spawnFox() {
+    let spawn = floor(random(burrows.length));
+
+    //check if the burrow is on an edge of the map, so the fox doesn't immediately go oob
+    let direction;
+    if(burrows[spawn].x >= width-200) {
+      direction = true;
+    }
+    else if(burrows[spawn].x <= 200) {
+      direction = false;
+    }
+    else {
+      direction = floor((random(0,1))+0.5);
+    }
+
+    let newFox = new Fox(foxID, burrows[spawn].x, burrows[spawn].y, direction, foxSpeed);
+    foxes.push(newFox);
+    activeFoxes++;
+    foxID++;
+  }
+
+  function deleteFox(del) {
+    for(let i = 0; i < foxes.length; i++) {
+      if(foxes[i].id === del) {
+        foxes.splice(i, 1);
+        print(`deleted ${del}`);
+        activeFoxes--;
+      }
+    }
+  }
+
+  function checkFoxBehavior() {
+    for(let i = 0; i < foxes.length; i++) {
+      switch(foxes[i].state) {
+        case `peek`: foxes[i].peeking();
+          break;
+        case `walk`: foxes[i].walking();
+          break;
+        case `eat`: foxes[i].eating();
+          break;
+        case `wait`: foxes[i].waiting();
+          break;
+        case `pet`: foxes[i].petting();
+          break;
+        case `burrow`: foxes[i].burrowing();
+          break;
+      }
+    }
+  }
+
+  function checkDifficulty() {
+
+  }
+
+  function checkLives() {
+    if(lives <= 0) {
+      bgColor.R = 0;
+      bgColor.G = 0;
+      bgColor.B = 0;
+      game = `gameOver`;
+    }
   }
 
   
@@ -137,6 +300,65 @@ class Burrow {
     //display everything in simulation
     imageMode(CENTER);
     rectMode(CENTER);
+
+    //display burrows
+    for(let i = 0; i < burrows.length; i++) {
+      image(burrowImg, burrows[i].x, burrows[i].y);
+    }
+
+    //display foxes
+    for(let i = 0; i < foxes.length; i++) {
+      switch(foxes[i].state) {
+        case `peek`:
+          if(foxes[i].goLeft) {
+            image(foxPeekL, foxes[i].x, foxes[i].y);
+          }
+          else {
+            image(foxPeekR, foxes[i].x, foxes[i].y);
+          }
+          break;
+        case `walk`: 
+          if(foxes[i].goLeft) {
+            image(foxWalkL, foxes[i].x, foxes[i].y);
+          }
+          else {
+            image(foxWalkR, foxes[i].x, foxes[i].y);
+          }
+          break;
+        case `eat`: 
+          if(foxes[i].goLeft) {
+            image(foxEatL, foxes[i].x, foxes[i].y);
+          }
+          else {
+            image(foxEatR, foxes[i].x, foxes[i].y);
+          }
+          break;
+        case `wait`: 
+          if(foxes[i].goLeft) {
+            image(foxWaitL, foxes[i].x, foxes[i].y);
+          }
+          else {
+            image(foxWaitR, foxes[i].x, foxes[i].y);
+          }
+          break;
+        case `pet`: 
+          if(foxes[i].goLeft) {
+            image(foxPetL, foxes[i].x, foxes[i].y);
+          }
+          else {
+            image(foxPetR, foxes[i].x, foxes[i].y);
+          }
+          break;
+        case `burrow`: 
+          if(foxes[i].goLeft) {
+            image(foxBurrowL, foxes[i].x, foxes[i].y);
+          }
+          else {
+            image(foxBurrowR, foxes[i].x, foxes[i].y);
+          }
+          break;
+      }
+    }
   }
 
   function gameOver() {
@@ -149,15 +371,14 @@ class Burrow {
   }
 
   function keyPressed() {
-    if(game === `title`) {
-      if (keyCode === ENTER) {
-        bgColor.R = 0;
-        bgColor.G = 0;
-        bgColor.B = 0;
+    if(keyCode === ENTER) {
+      if(game === `title`) {
+        bgColor.R = 88;
+        bgColor.G = 210;
+        bgColor.B = 88;
         game = `simulation`;
-      }
-    } else if(game === `gameOver`) {
-      if (keyCode === ENTER) {
+      } 
+      else if(game === `gameOver`) {
         location.reload()
       }
     }
